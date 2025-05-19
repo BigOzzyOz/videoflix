a7e6c26c2e20306348bd2ffa568c7cd9882bb77e
Jan Holtschke <60309436+BigOzzyOz@users.noreply.github.com>
2025-05-19 02:07:04 +0200

    feat: add landing, legal notice, login, player, register, and slider components with basic templates and styles
    - Created LandingComponent with template and spec.
    - Created LegalNoticeComponent with template and spec.
    - Created LoginComponent with template and spec.
    - Created PlayerComponent with template and spec.
    - Created RegisterComponent with template and spec.
    - Created SliderComponent with template and spec.
    - Added ApiService with basic structure and spec.
    - Updated pnpm workspace configuration to include only built dependencies.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LegalNoticeComponent } from './components/legal-notice/legal-notice.component';
+import { LoginComponent } from './components/login/login.component';
+import { LandingComponent } from './components/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'landing',
+        component: LandingComponent,
+        data: {
+            title: 'Videoflix'
+        }
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        data: {
+            title: 'Videoflix Login'
+        }
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        data: {
+            title: 'Videoflix Register'
+        }
+    },
+    {
+        path: 'imprint',
+        component: LegalNoticeComponent,
+        data: {
+            title: 'Videoflix Imprint'
+        }
+    },
+    {
+        path: 'privacy',
+        component: LegalNoticeComponent,
+        data: {
+            title: 'Videoflix Privacy Policy'
+        }
+    },
+    {
+        path: '',
+        redirectTo: 'landing',
+        pathMatch: 'full'
+    },
+];
